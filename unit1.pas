f4a2f4abcf79c159861abac7ac193cacc95846f5
Hevanafa <dhevansafa4@gmail.com>
2026-02-06 10:47:57 +0700

    added dictionary loader

--- unit1.pas
+++ unit1.pas
@@ -19,8 +19,11 @@ type
     SearchButton: TButton;
     InputEdit: TEdit;
     ResultMemo: TMemo;
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
     procedure InputEditKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
   private
+    rawWordlist: TStringList;
 
   public
 
@@ -38,6 +41,39 @@ implementation
 procedure TForm1.InputEditKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
 begin
   if key = VK_RETURN then ResultMemo.Text := InputEdit.text;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  f: text;
+  line: string;
+begin
+  rawWordlist := TStringList.create;
+
+  if not FileExists('TWL06.txt') then begin
+    ResultMemo.Text := 'Couldn''t find the dictionary file TWL06.txt!';
+    ResultMemo.Enabled := false;
+    exit
+  end;
+
+  AssignFile(f, 'TWL06.txt');
+  {$I-} Reset(f); {$I+}
+
+  while not EOF(f) do
+  begin
+    readln(f, line);
+    rawWordlist.Add(line);
+  end;
+
+  closeFile(f);
+
+  { TODO: Process the frequency list }
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  rawWordlist.Clear;
+  FreeAndNil(rawWordlist);
 end;
 
 end.
