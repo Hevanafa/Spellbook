a4ad78c78d095d7ce41b705e01073c7e615509b7
Hevanafa <dhevansafa4@gmail.com>
2026-02-06 11:02:10 +0700

    added frequency list processing

--- unit1.pas
+++ unit1.pas
@@ -3,17 +3,17 @@ unit Unit1;
 {$Mode ObjFPC}
 {$J-}
 {$H-}
+{$Notes OFF}
 
 interface
 
 uses
   Classes, SysUtils, Forms, Controls,
   Graphics, Dialogs, StdCtrls,
-  LCLType;
+  LCLType, FGL;
 
 type
-
-  { TForm1 }
+  TLetterFreqMap = specialize TFPGMap<char, smallint>;
 
   TForm1 = class(TForm)
     SearchButton: TButton;
@@ -22,8 +22,10 @@ type
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure InputEditKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+
   private
     rawWordlist: TStringList;
+    frequencyMap: specialize TFPGMap<string, TLetterFreqMap>;
 
   public
 
@@ -40,13 +42,16 @@ implementation
 
 procedure TForm1.InputEditKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
 begin
-  if key = VK_RETURN then ResultMemo.Text := InputEdit.text;
+  if key = VK_RETURN then
+    ResultMemo.Text := UpperCase(InputEdit.text);
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 var
   f: text;
-  line: string;
+  line, term: string;
+  c: char;
+  newFreqMapEntry: TLetterFreqMap;
 begin
   rawWordlist := TStringList.create;
 
@@ -69,7 +74,18 @@ begin
 
   ResultMemo.Text := 'Loaded ' + inttostr(rawWordlist.count) + ' words';
 
-  { TODO: Process the frequency list }
+  { Process the frequency list }
+  for term in rawWordlist do begin
+    newFreqMapEntry := TLetterFreqMap.create;
+
+    for c in term do
+      if newFreqMapEntry.indexOf(c) >= 0 then begin
+        newFreqMapEntry[c] := newFreqMapEntry[c] + 1
+      end else
+        newFreqMapEntry.Add(c, 1);
+
+    frequencyMap.Add(term, newFreqMapEntry);
+  end;
 end;
 
 procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
